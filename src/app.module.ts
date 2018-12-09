e54b742c15aed803125d6593bbe60872772e6128
songlairui <cnre@live.cn>
2018-12-09 21:25:04 +0800

    add loggerMiddleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
 
 @Module({
   imports: [CatsModule],
@@ -9,3 +10,9 @@ import { CatsModule } from './cats/cats.module';
   providers: [AppService],
 })
 export class AppModule {}
+
+export class ApplicationModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('cats');
+  }
+}
