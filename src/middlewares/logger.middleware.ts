3d7cad65d6c6ff36b6ad26670780091419dc8049
songlairui <cnre@live.cn>
2018-12-16 23:40:13 +0800

    添加 log

--- src/middlewares/logger.middleware.ts
+++ src/middlewares/logger.middleware.ts
@@ -2,8 +2,10 @@ import { Injectable, NestMiddleware, MiddlewareFunction } from '@nestjs/common';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
-  resolve(name: string): MiddlewareFunction {
-    return (req, res, next) => {
+  async resolve(name: string): Promise<MiddlewareFunction> {
+    await new Promise(r => setTimeout(r, 234));
+    return async (req, res, next) => {
+      await new Promise(r => setTimeout(r, 1234));
       console.log('Request...', name);
       next();
     };
